b3716920c49c2e8c125c8b8ba95b2d07d34d40f0
Andrew Gergert <axedrew33@gmail.com>
2019-07-07 17:45:36 +0600

    Add an example module

---
+++ modules/sinu5oid.py
@@ -0,0 +1,26 @@
+from random import randint
+
+def bubbleSort(list):
+    # to not mutate input list
+    result = list.copy()
+    for i in range(len(result)):
+        for j in range(i + 1, len(result)):
+            if result[i] > result[j]:
+                result[i], result[j] = result[j], result[i]
+    return result
+
+def generateList(len):
+    return [randint(0, 10) for i in range(len)]
+
+# if runs as standalone module
+if __name__ == "__main__":
+    list = generateList(10)
+
+    print('Unsorted list')
+    print(list)
+
+    sorted_list = bubbleSort(list)
+    print('Sorted list')
+    print(sorted_list)
+    print('Origin list didn\'t change')
+    print(list)

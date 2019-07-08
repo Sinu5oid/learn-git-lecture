8850ee1bd702c3ee3088fe7c16ce44b7a1857ea0
Andrew Gergert <axedrew33@gmail.com>
2019-07-08 12:03:34 +0600

    Changed list length

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import modules.sinu5oid as sinu5oid
 if __name__ == "__main__":
     # Add your method calls below here:
     
-    list = sinu5oid.generateList(10)
+    list = sinu5oid.generateList(15)
     print(list)
     print(sinu5oid.bubbleSort(list))
     # and above here
